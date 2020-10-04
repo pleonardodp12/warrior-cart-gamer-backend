b37d189cc7858d30f3281a6db167af4b5a861cd0
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-10-04 14:26:10 -0300

    transfer backend

---
+++ src/server.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const shortid = require('shortid')
+const cors = require('cors')
+const { config } = require('dotenv')
+const { join } = require('path')
+
+const path = join(__dirname, '.env')
+config({path})
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const Product = mongoose.model("products", new mongoose.Schema({
+  _id: { type: String, default: shortid.generate },
+  name: String,
+  image: String,
+  description: String,
+  price: Number,
+  brand: String,
+  rating: Number,
+  numReviews: Number,
+}));
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) =>{
+  const newProducts = new Product(req.body);
+  const savedProduct = await newProducts.save();
+  res.send(savedProduct);
+});
+
+app.delete("/api/products/:id", async(req, res)=>{
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+const Order = mongoose.model("order", new mongoose.Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  email: String,
+  name: String,
+  address: String,
+  total: Number,
+  cartItems: [
+    {
+      _id: String,
+      name: String,
+      price: Number,
+      count: Number
+    },
+  ],
+}, {
+  timestamps: true,
+}
+
+));
+
+app.post("/api/orders", async(req, res) => {
+  if(!req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ){
+    return res.send({message: 'Preencha as informações'})
+  }
+  const order = await Order(req.body).save();
+  res.send(order)
+
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(" server rodando em http://localhost:5000"))
